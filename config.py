b91198db79761096dca0b38341bd619643dde2b4
owenorcan <owenorcan@gmail.com>
2023-11-07 14:30:29 -0500

    Comments
    Added comments to explain what is happening

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
 from getpass import getpass
 from cryptography.fernet import Fernet
 
+# Function for writing a key to encrypt and decrypt the admin password.
 def write_key():
     key = Fernet.generate_key()
     with open("key2.key", "wb") as key_file:
         key_file.write(key)
 
+# Function for reading the file that the key is stored and storing the key in a variable
 def load_admin_key():
     file = open("key2.key", "rb")
     key = file.read()
     file.close()
     return key 
 
+# Checking if there is a key already created, if not create a key 
 with open("key2.key", "r") as check:
     checkfile = check.read()
     if checkfile == "":
@@ -20,8 +23,8 @@ with open("key2.key", "r") as check:
 key = load_admin_key()
 fer = Fernet(key)
 
-
-def decrypt_admin_key():
+# decrypting the admin password (if the admin password is not set, does nothing)
+def decrypt_admin_pwd():
     try:
         with open("admin.key", "r") as f:
             data = f.read()
@@ -31,14 +34,17 @@ def decrypt_admin_key():
         pass
 
     
-admin = decrypt_admin_key()
+admin = decrypt_admin_pwd()
 
+# If there is already an admin password set put the current admin password in a variable
 try:
     with open("admin.key", "r") as ac:
         admincheck = ac.read()
+# If there is no file called admin.key, pass this part (means that the admin password has not been set yet)
 except FileNotFoundError:
     pass
-try:
+# If the variable from "line 42" is not equal to an empty string (nothing), get input from user and compare the input to the decrypted admin password
+try: 
     if admincheck != "":
         admin_pwd = str(getpass("Enter your current admin password to continue: "))
         if admin_pwd != admin:
@@ -46,7 +52,9 @@ try:
             exit()
 except NameError:
     pass
-    
+
+
+# Main Interface to set password (and encrypt it)
 while True:
     admin_passw = getpass("What is your new admin password? ")
     if len(admin_passw) < 2:
--- passmanager.py
+++ passmanager.py
@@ -1,33 +1,37 @@
-#PREPREQUISITES: 
-#pip install cryptio
-#pip install fernet
 from getpass import getpass
 from cryptography.fernet import Fernet
 
+############################# Admin Password Part ##########################
+
+#check the file for encryption key for admin password
 with open("key2.key", "r") as kc:
     keycheck = kc.read()
-
+# if there is no key in the file exit the program
 if keycheck == "":
     print("Please run the config file first to set your admin password")
     exit()
-
+# put the admin encryption key inside a variable
 def load_admin_key():
     file = open("key2.key", "rb")
     key = file.read()
     file.close()
     return key
 
+
 admin_key = load_admin_key()
 admin_fer = Fernet(admin_key)
 
+# decrypt the admin password using the key from the load_admin_key function
 def decrypt_admin_key():
     with open("admin.key", "r") as f:
         data = f.read()
         passw = admin_fer.decrypt(data.encode()).decode()
         return passw
 
+# put the decrpted admin password inside a variable to compare it to the input
 admin = decrypt_admin_key()
 
+# compare the input to the  the admin password
 while True:
     adm_psw = str(getpass("What is the admin password: (Press q to exit) "))
     if adm_psw == "q":
@@ -38,11 +42,15 @@ while True:
     elif adm_psw == admin:
         break
 
+############################# Password Manager Part ##############################
+# Function to make a key to encrypt and decrypt the passwords and storing the key inside key.key
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
 
+
+# Checking if a key has already been set, if not create a key.
 with open("key_check.lock", "r") as readcheck:
     readed = readcheck.read()
 if readed == "True":
@@ -52,6 +60,7 @@ else:
         write_key()
         check.write("True")
 
+# Function to get the key from key.key and store it in a variable.
 def load_key():
     file = open("key.key", "rb")
     key = file.read()
@@ -62,25 +71,28 @@ def load_key():
 key = load_key()
 fer = Fernet(key)
  
-
+# Function for viewing the saved Accounts and Passwords.
 def view():
     with open('passwords.lock', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             platform, user, passw = data.split("|")
 
+            # Printing the decrpyted data.
             print("Platform:", platform, "User:", user, "Password:",
                 fer.decrypt(passw.encode()).decode())
 
+# Function for adding new account data.
 def add():  
     plf = input("Platform: ")
     name = input("Account name: ")
     pwd = input("Password: ")
 
+    # encrypting the inputted password and storing it inside passwords.lock
     with open('passwords.lock', 'a') as f:
         f.write(plf + "|" + name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
-
+# Main Interface
 while True:
     mode = input("Would you like to add a password or view existing ones (view,add)? press q to quit: ").lower()
     if mode == "q":
